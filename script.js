5f64b50375be879b4611c28cab0d83b94fc49d84
Yevgeniiaa <138613772+Yevgeniiaa@users.noreply.github.com>
2024-11-13 22:58:56 +0200

    array

---
+++ script.js
@@ -0,0 +1,113 @@
+let executes = document.querySelectorAll(".execute")
+{
+    let arr = [1,2,3,4,5]
+    executes[0].textContent = arr.length
+}
+
+{
+    let arr = [1,2,3,4,5]
+    executes[1].textContent = arr.toString ()
+}
+
+{
+    let arr = [1,2,3,4,5]
+    executes[2].textContent = arr.join ()
+}
+
+{
+    let arr = [1,2,3,4,5]
+    executes[3].textContent = arr.pop ()
+}
+{
+    let arr = [1,2,3,4,5]
+    executes[4].textContent = arr.push ()
+}
+{
+    let arr = [1,2,3,4,5]
+    executes[5].textContent = arr.shift ()
+}
+{
+    let arr = [1,2,3,4,5]
+    executes[6].textContent = arr.unshift ()
+}
+{
+    let arr = [1,2,3,4,5]
+    executes[7].textContent = arr.splice (start)
+}
+{
+    let arr = [1,2,3,4,5]
+    executes[8].textContent = arr.concat ()
+}
+{
+    let arr = [1,2,3,4,5]
+    executes[9].textContent = arr.slice ()
+}
+{
+    let arr = [1,2,3,4,5]
+    executes[10].textContent = arr.sort ()
+}
+{
+    let arr = [1,2,3,4,5]
+    executes[11].textContent = arr.reserve ()
+}
+{
+    let arr = [1,2,3,4,5]
+    executes[12].textContent = arr.random ()
+}
+{
+    let arr = [1,2,3,4,5]
+    executes[13].textContent = arr.copyWithin ()
+}
+{
+    let arr = [1,2,3,4,5]
+    executes[14].textContent = arr.every ()
+}
+{
+    let arr = [1,2,3,4,5]
+    executes[15].textContent = arr.fill ()
+}
+{
+    let arr = [1,2,3,4,5]
+    executes[16].textContent = arr.filter ()
+}
+{
+    let arr = [1,2,3,4,5]
+    executes[17].textContent = arr.find ()
+}
+{
+    let arr = [1,2,3,4,5]
+    executes[18].textContent = arr.findIndex ()
+}
+
+{
+    let arr = [1,2,3,4,5]
+    executes[19].textContent = arr.forEach ()
+}
+{
+    let arr = [1,2,3,4,5]
+    executes[20].textContent = arr.indexOf ()
+}
+{
+    let arr = [1,2,3,4,5]
+    executes[21].textContent = arr.isArray ()
+}
+{
+    let arr = [1,2,3,4,5]
+    executes[22].textContent = arr.lastIndexOf ()
+}
+{
+    let arr = [1,2,3,4,5]
+    executes[23].textContent = arr.reduceRight ()
+}
+{
+    let arr = [1,2,3,4,5]
+    executes[24].textContent = arr.reverse ()
+}
+{
+    let arr = [1,2,3,4,5]
+    executes[25].textContent = arr.some ()
+}
+{
+    let arr = [1,2,3,4,5]
+    executes[26].textContent = arr.valueOf ()
+}
